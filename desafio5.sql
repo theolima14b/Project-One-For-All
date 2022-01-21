287451af00e668ecc5d7b90ac200bbe46378efba
Theo Lima <theo.suporte@hotmail.com>
2022-01-21 19:24:44 -0300

    Implementa desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
--- SELECT 
--- 	M.music_name AS 'cancao', 
---   COUNT(H.music) AS 'reproducoes' 
--- FROM SpotifyClone.listening_history AS H
--- INNER JOIN SpotifyClone.musics AS M
--- 	ON H.music = M.music_id 
--- GROUP BY H.music
--- ORDER BY COUNT(H.music) DESC, M.music_name
--- LIMIT 2; 
+SELECT 
+	M.music_name AS 'cancao', 
+  COUNT(H.music) AS 'reproducoes' 
+FROM SpotifyClone.listening_history AS H
+INNER JOIN SpotifyClone.musics AS M
+	ON H.music = M.music_id 
+GROUP BY H.music
+ORDER BY COUNT(H.music) DESC, M.music_name
+LIMIT 2; 
