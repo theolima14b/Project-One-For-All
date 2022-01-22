c5abe5be030143bf4ecb03598f2b447b508c2269
Theo Lima <theo.suporte@hotmail.com>
2022-01-21 23:14:01 -0300

    Conserta erro

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT COUNT(H.`user`) AS 'quantidade_musicas_no_historico'
-FROM SpotifyClone.listening_history AS H 
+FROM SpotifyClone.history AS H 
 INNER JOIN SpotifyClone.users AS U
 ON H.`user` = U.user_id
 WHERE U.`name` LIKE 'Bill';
